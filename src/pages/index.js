1eac256bd2e150a60dba48f4513fd3c539459be0
Drew Wei <drewwei0923@gmail.com>
2023-05-01 14:01:27 -0500

    Added Announcements section.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,32 @@
+import { graphql } from "gatsby";
 import { AboutSection, ContactSection, HeroSection, Page, Seo } from "gatsby-theme-portfolio-minimal";
 import React from "react";
 
-export default function IndexPage() {
+import { AnnouncementsSection } from "src/components/sections/AnnouncementsSection";
+
+export default function IndexPage({ data }) {
   return (
     <>
       <Seo title="NAW Database" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <AboutSection sectionId="about" heading="About" />
+        <AnnouncementsSection sectionId="announcements" heading="Announcements" data={data.allAnnouncementsCsv.nodes} />
         <ContactSection sectionId="github" heading="Issues?" />
       </Page>
     </>
   );
 }
+
+export const pageQuery = graphql`
+  query pageQuery {
+    allAnnouncementsCsv {
+      nodes {
+        id
+        type
+        date
+        text
+      }
+    }
+  }
+`;
