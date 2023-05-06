72b99d12dcefa20d77a8efe292586f35f7975c9a
Drew Wei <drewwei0923@gmail.com>
2023-05-06 01:51:29 -0500

    Debug

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://0.0.0.0:8000",
+    baseUrl: "http://localhost:8000",
     specPattern: "cypress/e2e",
     supportFile: "cypress/support/e2e.ts",
     video: false
